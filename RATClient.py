fa5b59f40cb7f57a2fbdd22a01c517fcba23bfcb
phansen01 <prestonhansen01@gmail.com>
2015-06-11 13:25:06 -0500

    Update RATClient.py

--- RATClient.py
+++ RATClient.py
@@ -1,33 +1,54 @@
 import sys
 import zmq
 import time
-import numpy as np
+#import numpy as np
+from threading import Thread
+from random import randint
 
 
-context = zmq.Context()
+#context = zmq.Context()
 
 port = '5555'
-socket = context.socket(zmq.REQ)
-socket.connect( "tcp://localhost:%s" % (port) )
+#socket = context.socket(zmq.REQ)
+#socket.connect( "tcp://localhost:%s" % (port) )
 
+NUMBER_OF_CLIENTS = 1 #doesn't scale properly yet
+def set_id(zsocket):
+    """Set simple random printable identity on socket"""
+    identity = u"%04x-%04x" % (randint(0, 0x10000), randint(0, 0x10000))
+    
+    zsocket.setsockopt_string(zmq.IDENTITY, identity)
 
-def Signal():
+def RAT_Thread(context=None):
+    context = context or zmq.Context.instance()
+    RATclient = context.socket(zmq.REQ)
+    set_id(RATclient)
+    RATclient.connect("tcp://localhost:5555")
+    if Signal(RATclient):
+        SendJob(RATclient)
+    else:
+        print "couldn't connect"
+        exit(1)
+    ReceiveInfo(RATclient)
+
+def Signal(socket):
     print "Sending signal at port %s"%(port)
-    socket.send(b"a")
+    socket.send(b"z")
     signal = socket.recv()
     print signal
     if signal is not None:
         socket.send(b"a")
         return True
-    
-    
-def SendJob():
+        
+def SendJob(socket):
     #identifier
     print socket.recv()
     socket.send(b"some int")
     print socket.recv()
     socket.send(b"test")
-def ReceiveInfo():
+# later on probably want to use multipart messages and something
+#like: if is_more: receive info, if signal == something sendJob  
+def ReceiveInfo(socket):
     signal = socket.recv()
     if signal == "DATA":
         socket.send(b"go")
@@ -37,12 +58,8 @@ def ReceiveInfo():
         print "New Data: ",newData1,"\nNew Data: ",newData2
 
 def main():
-    if Signal():
-        SendJob()
-    else:
-        print "couldn't connect"
-        exit(1)
-    ReceiveInfo()
+    for i in range(NUMBER_OF_CLIENTS):
+        Thread(target=RAT_Thread).start()
 
 if __name__ == "__main__":
     main()
