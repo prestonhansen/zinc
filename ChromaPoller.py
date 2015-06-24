9dde67b30ff84eaa8739ef7fd6ef8c0726030222
Preston Hansen <prestonhansen01@gmail.com>
2015-06-23 23:21:21 -0500

    WIP: ChromaPoller initializes sim (want to use cerenkov photons from RAT)

--- ChromaPoller.py
+++ ChromaPoller.py
@@ -1,16 +1,20 @@
-import sys
+import os, sys, time
 import zmq
-import time
+import chroma.api as api
+api.use_cuda()
 
+import numpy as np
+from chroma.sim import Simulation
+from chroma.event import Photons
+import chroma.event
 
+from uboone import uboone
 
 context = zmq.Context().instance()
-
 
 backend = context.socket(zmq.REP)
 
 backport = '5556' #talks to REQ frontend of server (on backend)
-
 
 backend.connect( "tcp://localhost:%s"%(backport) )
 
@@ -23,15 +27,26 @@ def main():
         socks = dict(poll_RAT.poll())
         
         if socks.get(backend) == zmq.POLLIN:
-            msg = backend.recv#_multipart()
+            msg = backend.recv()#_multipart()
             #^decide whether to use multipart or not^
             if not msg:
                 break
             
+                
             #pack received data into numpy array
 
             #ship to GPU, do some stuff, send data back
-
+            det = uboone()
             
+            sim = Simulation(det,geant4_processes=0,nthreads_per_block = 1, max_blocks = 1024)
+            #get photons here, pack them, run sim
+            nphotons = -1 #need to grab count from rat
+            
+            photons = -1 #get these from RAT (cerenkov)
+            events = sim.simulate(photons, keep_photons_end=True, max_steps=2000)
+            
+            for ev in events:
+                nhits = ev.channels.hit["""something goes here"""]
+                
 if __name__ = "__main__":
     main()
