e058b8501ff4974c4f5a712687e207e4a71a34e3
Taritree Wongjirad <tmw@oppenheimer.fnal.gov>
2015-06-25 16:13:27 -0500

    saving work on oppen.; want to use scintstep data in chroma

--- ChromaPoller.py
+++ ChromaPoller.py
@@ -1,5 +1,5 @@
 import os, sys, time
-import zmq
+nimport zmq
 import chroma.api as api
 api.use_cuda()
 import numpy as np
@@ -30,7 +30,7 @@ sim = Simulation(det,geant4_processes=0,nthreads_per_block = 1, max_blocks = 102
 
 def main():
     while True:
-        socks = dict(poll_RAT.poll())
+        socks = dict(poll_Server.poll())
         
         if socks.get(backend) == zmq.POLLIN:
             msg = backend.recv()#_multipart()
@@ -40,38 +40,45 @@ def main():
             
             chromaData = ratchromadata_pb2.ChromaData()
             chromaData.ParseFromString(msg)
+            print chromaData
+            nphotons = sum(1 for p in enumerate(chromaData.cherenkovdata))
+            dphi = np.random.uniform(0,2.0*np.pi, nphotons)
+            dcos = np.random.uniform(-1.0, 1.0, nphotons)
+            dir = np.array( zip( np.sqrt(1-dcos[:]*dcos[:])*np.cos(dphi[:]), np.sqrt(1-dcos[:]*dcos[:])*np.sin(dphi[:]), dcos[:] ), dtype=np.float32 )
+
+            print "NUMPHOTONS: ",nphotons
+            """for cherenkov photons"""
+            # dir = np.zeros((nphotons,3), 
+            #               dtype = np.float32)
+            # for i,cData in enumerate(chromaData.cherenkovdata):
+            #   dir[i,0] = cData.dx()
+            #     dir[i,1] = cData.dy()
+            #     dir[i,2] = cData.dz()
+            # pol = np.zeros_like(dir)
+            # for i,cData in (chromaData.cherenkovdata):
+            #     pol[i,0] = cData.px()
+            #     pol[i,1] = cData.py()
+            #     pol[i,2] = cData.pz()
             
-            dir = np.zeros((len(chromaData.cherenkovdata),3), 
-                           dtype = np.float32)
-            for i,cData in enumerate(chromaData.cherenkovdata):
-                dir[i,0] = cData.dx()
-                dir[i,1] = cData.dy()
-                dir[i,2] = cData.dz()
-            pol = np.zeros_like(dir)
-            for i,cData in (chromaData.cherenkovdata):
-                pol[i,0] = cData.px()
-                pol[i,1] = cData.py()
-                pol[i,2] = cData.pz()
+            # pos = np.zeros_like(dir)
+            # for i,cData in (chromaData.cherenkovdata):
+            #     pos[i,0] = cData.x()
+            #     pos[i,1] = cData.y()
+            #     pos[i,2] = cData.z()
             
-            pos = np.zeros_like(dir)
-            for i,cData in (chromaData.cherenkovdata):
-                pos[i,0] = cData.x()
-                pos[i,1] = cData.y()
-                pos[i,2] = cData.z()
+            # t = np.zeros((nphotons), 
+            #              dtype=np.float32)
+            # for i,cData in (chromaData.cherenkovdata):
+            #     t[i] = cData.t()
             
-            t = np.zeros((len(chromaData.cherenkovdata)), 
-                         dtype=np.float32)
-            for i,cData in (chromaData.cherenkovdata):
-                t[i] = cData.t()
-            
-            wavelength = np.zeros_like(t)
-            for i,cData in (chromaData.cherenkovdata):
-                wavelength[i] = cData.wavelength()
+            # wavelengths = np.zeros_like(t)
+            # for i,cData in (chromaData.cherenkovdata):
+            #     wavelengths[i] = cData.wavelengths()
 
             
             #ship to GPU, do some stuff, send data back
             photons = Photons(pos=pos, dir=dir, pol=pol, t=t,
-                              wavelength = wavelength)
+                              wavelengths = wavelengths)
 
             events = sim.simulate(photons, keep_photons_end=True, max_steps=2000)
             
@@ -81,7 +88,7 @@ def main():
                 channelhit[:] = det.solid_id_to_channel_index[ det.solid_id[ev.photons_end.last_hit_triangles[:] ] ]
                 for n,f in enumerate(detected_photons):
                     if f!=0:
-                        print "hit detID":,channelhit[n]," pos=",ev.photons_end.pos[n,:]," time=",ev.photons_end.t[n]-100.0
+                        print "hit detID:",channelhit[n]," pos=",ev.photons_end.pos[n,:]," time=",ev.photons_end.t[n]-100.0
                 
 if __name__ == "__main__":
     main()
