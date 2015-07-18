702f3f0795394eeecaba68972b6db86db0ca6a3f
phansen01 <prestonhansen01@gmail.com>
2015-07-18 11:54:45 -0500

    wrapped in namespace

--- photonMessage.hh
+++ photonMessage.hh
@@ -3,19 +3,19 @@
 #include <zmq.hpp>
 #include <string>
 
-
-void C_MessagePack(int* PMTArr, float* TimeArr, float* WaveArr, float* PosArr, float* DirArr, float* PolArr, int nphotons);
-//  Sends string as 0MQ string
-bool s_send (zmq::socket_t & socket, const std::string & string);
-
-//  Receive 0MQ string from socket and convert into C string
-//  Caller must free returned string. Returns NULL if the context
-//  is being terminated.
-char * s_recv (void *socket);
-
-void shipBack();
-void killSocket();
-std::string returnPhits();
-
+namespace Message_Packing{
+  void C_MessagePack(int* PMTArr, float* TimeArr, float* WaveArr, float* PosArr, float* DirArr, float* PolArr, int nphotons);
+  //  Sends string as 0MQ string
+  bool s_send (zmq::socket_t & socket, const std::string & string);
+  
+  //  Receive 0MQ string from socket and convert into C string
+  //  Caller must free returned string. Returns NULL if the context
+  //  is being terminated.
+  char * s_recv (void *socket);
+  
+  void shipBack();
+  void killSocket();
+  std::string returnPhits();
+}
 
 
